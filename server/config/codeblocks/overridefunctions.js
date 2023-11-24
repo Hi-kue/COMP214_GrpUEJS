b71622c656c7d09eb27f1739da520ed2b93a6083
maljelma <133683358+maljelma@users.noreply.github.com>
2023-11-23 21:30:42 -0500

    security applied

--- server/config/codeblocks/overridefunctions.js
+++ server/config/codeblocks/overridefunctions.js
@@ -13,13 +13,13 @@ let originalRender = express.response.render;
 express.response.render = function(view, locals, callback) {
     // ...
     console.log('rendering:',view);
-    console.log('user check skipped. /server/config/codeblock/overridefunction.js');
+    // console.log('user check skipped - for debuging. /server/config/codeblock/overridefunction.js');
     // make sure there is a user, or the page is login or rigister page.
-    /* if (this.req.user || view === 'login' || view === 'register'){
+    if (this.req.user || view === 'login' || view === 'register'){
         originalRender.call(this, view, locals, callback);
     } else {
         originalRender.call(this, 'login', locals, callback);
-    } */
+    }
     // call the original render function
     originalRender.call(this, view, locals, callback);
 };
